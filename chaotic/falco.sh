bd5d2ad35f01b4bf24ffa3a7fae1d211128704df
Max Watermolen <maxw@maxwelldps.com>
2024-11-10 16:04:45 -0800

    cheges idk

--- chaotic/falco.sh
+++ chaotic/falco.sh
@@ -23,9 +23,6 @@ install_falco(){
 	sudo systemctl unmask falcoctl-artifact-follow.service
 	sudo systemctl enable falcoctl-artifact-follow.service
 
-    # Show falco status
-	sudo systemctl list-units | grep falco
-
 	# Falco configuration for rootless Docker
 	sudo cp $CHAOS_DIR/etc/falco/config.d/01-chaos.yaml     /etc/falco/config.d/01-chaos.yaml
     sudo sed -i -e "s;CHAOS_DIR;$CHAOS_DIR;g"               /etc/falco/config.d/01-chaos.yaml
